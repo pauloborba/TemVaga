2f4d08de5bc7365306830d3b75122b68fbbf141c
Igor Simoes <ibps@cin.ufpe.br>
2020-11-10 14:44:12 -0300

    Create server folder

---
+++ server-tem-vaga/tem-vaga-server.ts
@@ -0,0 +1,63 @@
+import express = require('express');
+import bodyParser = require('body-parser');
+
+var tvserver = express();
+
+var allowCrossDomain = function (req: any, res: any, next: any) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+
+tvserver.use(allowCrossDomain);
+
+tvserver.use(bodyParser.json());
+
+// tvserver.get('/alunos', function (req: express.Request, res: express.Response) {
+//   res.send(JSON.stringify(cadastro.getAlunos()));
+// })
+
+// tvserver.post('/aluno', function (req: express.Request, res: express.Response) {
+//   var aluno: Aluno = <Aluno> req.body; //verificar se é mesmo Aluno!
+//   aluno = cadastro.cadastrar(aluno);
+//   if (aluno) {
+//     res.send({"success": "O aluno foi cadastrado com sucesso"});
+//   } else {
+//     res.send({"failure": "O aluno não pode ser cadastrado"});
+//   }
+// })
+
+// tvserver.put('/aluno', function (req: express.Request, res: express.Response) {
+//   var aluno: Aluno = <Aluno> req.body;
+//   aluno = cadastro.atualizar(aluno);
+//   if (aluno) {
+//     res.send({"success": "O aluno foi atualizado com sucesso"});
+//   } else {
+//     res.send({"failure": "O aluno não pode ser atualizado"});
+//   }
+// })
+
+// tvserver.delete('/aluno/:cpf', function (req: express.Request, res: express.Response) {
+//     let position = cadastro.deletar(req.params.cpf)
+//     if (position !== -1) {
+//       res.send({ success: "O aluno foi deletado com sucesso", alunoIndex: position});
+//     } else {
+//       res.send({"failure": "O aluno não pode ser deletado"});
+//   }
+// })
+
+// function clearServer(): void {
+//   cadastro = new CadastroDeAlunos();
+// }
+
+// function closeServer(): void {
+//   server.close();
+// }
+
+var server: any = tvserver.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+// export { server, closeServer, clearServer };
+export { server };
