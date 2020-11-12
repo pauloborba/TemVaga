7e79306b977ef8aa297f2f050ccd4c849a51b1df
Gaudiot <victor.gaudiot@gmail.com>
2020-11-12 17:51:29 -0300

    route class basics

--- common/route.ts
+++ common/route.ts
@@ -1,5 +1,29 @@
-export default class Route{
-    constructor(){
+interface IPlace{
+    street: string;
+    number: number;
+}
 
+interface IRoute{
+    departurePlace: IPlace;
+    arrivalPlace: IPlace;
+}
+
+export default class Route{
+    departurePlace: IPlace;
+    arrivalPlace: IPlace;
+    stops: IPlace[];
+
+    constructor({departurePlace, arrivalPlace}: IRoute){
+        this.departurePlace = departurePlace;
+        this.arrivalPlace = arrivalPlace;
+    }
+
+    addStop(stop: IPlace){
+        this.stops.push(stop);
+    }
+
+    removeStop(stop: IPlace){
+        const stopIndex = this.stops.findIndex(s => s == stop);
+        this.stops.splice(stopIndex, 1);
     }
 }
