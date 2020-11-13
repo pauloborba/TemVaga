eae033defefac509c33d88d23a723c7bc1267b3f
Igor Simoes <ibps@cin.ufpe.br>
2020-11-13 15:22:09 -0300

    Remove unnecessary comments from server file

--- server-tem-vaga/tem-vaga-server.ts
+++ server-tem-vaga/tem-vaga-server.ts
@@ -20,50 +20,8 @@ tvserver.use('/carApi', require('./src/routes/car.api'));
 
 tvserver.use('/rideApi', require('./src/routes/ride.api'));
 
-// tvserver.get('/alunos', function (req: express.Request, res: express.Response) {
-//   res.send(JSON.stringify(cadastro.getAlunos()));
-// })
-
-// tvserver.post('/aluno', function (req: express.Request, res: express.Response) {
-//   var aluno: Aluno = <Aluno> req.body; //verificar se é mesmo Aluno!
-//   aluno = cadastro.cadastrar(aluno);
-//   if (aluno) {
-//     res.send({"success": "O aluno foi cadastrado com sucesso"});
-//   } else {
-//     res.send({"failure": "O aluno não pode ser cadastrado"});
-//   }
-// })
-
-// tvserver.put('/aluno', function (req: express.Request, res: express.Response) {
-//   var aluno: Aluno = <Aluno> req.body;
-//   aluno = cadastro.atualizar(aluno);
-//   if (aluno) {
-//     res.send({"success": "O aluno foi atualizado com sucesso"});
-//   } else {
-//     res.send({"failure": "O aluno não pode ser atualizado"});
-//   }
-// })
-
-// tvserver.delete('/aluno/:cpf', function (req: express.Request, res: express.Response) {
-//     let position = cadastro.deletar(req.params.cpf)
-//     if (position !== -1) {
-//       res.send({ success: "O aluno foi deletado com sucesso", alunoIndex: position});
-//     } else {
-//       res.send({"failure": "O aluno não pode ser deletado"});
-//   }
-// })
-
-// function clearServer(): void {
-//   cadastro = new CadastroDeAlunos();
-// }
-
-// function closeServer(): void {
-//   server.close();
-// }
-
 var server: any = tvserver.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
-// export { server, closeServer, clearServer };
 export { server };
