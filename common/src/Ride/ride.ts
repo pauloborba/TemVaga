e01c75d80131dcb89c3cc33d6e7ef1d045c61545
Igor Simoes <ibps@cin.ufpe.br>
2020-11-14 19:23:39 -0300

    Fix missing properties on common classes

--- common/src/Ride/ride.ts
+++ common/src/Ride/ride.ts
@@ -15,21 +15,23 @@ const baseIPlace:IPlace = {street: '', number: 0};
 
 export default class ride{
     id: string;
-    driver: User;
+    driver: string;
     price: number;
     isPrivate: boolean;
     departureTime: Date;
     route: Route;
     seats: Seats;
+    passengers: string[];
 
-    constructor(driver: User, {price, places, isPrivate, departureTime}: IRide){
+    constructor(driver: string, {price, places, isPrivate, departureTime}: IRide){
         this.id = uuid();
-        this.driver = driver.clone();
+        this.driver = driver;
         this.price = price;
         this.route = new Route({departurePlace: baseIPlace, arrivalPlace: baseIPlace});
         this.seats = new Seats(places);
         this.isPrivate = isPrivate;
         this.departureTime = departureTime;
+        this.passengers = [];
     }
 
     changePrivacy(){
