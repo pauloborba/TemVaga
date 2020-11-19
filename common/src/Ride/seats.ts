47fc510068e4073da3abffd6abcf9a2706c157ed
Igor Simoes <ibps@cin.ufpe.br>
2020-11-19 05:05:29 -0300

    Fix identation for ride related classes

--- common/src/Ride/seats.ts
+++ common/src/Ride/seats.ts
@@ -1,28 +1,28 @@
-export default class Seats{
-    qttPlacesAvailable: number;
-    seats: string[] = [];
-    requests: string[] = [];
+export default class Seats {
+  qttPlacesAvailable: number;
+  seats: string[] = [];
+  requests: string[] = [];
 
-    constructor(places: number){
-        this.qttPlacesAvailable = places;
-    }
+  constructor(places: number) {
+    this.qttPlacesAvailable = places;
+  }
 
-    makeRequest(cpf: string): void{
-        this.requests.push(cpf);
-    }
+  makeRequest(cpf: string): void {
+    this.requests.push(cpf);
+  }
 
-    acceptRequest(cpf: string): void{
-        const userIndex = this.requests.findIndex(c => c === cpf);
-        if(userIndex === -1) throw new Error();
-        this.seats.push(cpf);
+  acceptRequest(cpf: string): void {
+    const userIndex = this.requests.findIndex(c => c === cpf);
+    if (userIndex === -1) throw new Error();
+    this.seats.push(cpf);
 
-        this.requests.splice(userIndex, 1);
-        this.qttPlacesAvailable -= 1;
-    }
+    this.requests.splice(userIndex, 1);
+    this.qttPlacesAvailable -= 1;
+  }
 
-    rejectRequest(cpf: string): void{
-        const userIndex = this.requests.findIndex(c => c === cpf);
-        if(userIndex === -1) throw new Error();
-        this.requests.splice(userIndex, 1);
-    }
+  rejectRequest(cpf: string): void {
+    const userIndex = this.requests.findIndex(c => c === cpf);
+    if (userIndex === -1) throw new Error();
+    this.requests.splice(userIndex, 1);
+  }
 }
